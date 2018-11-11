3c0fe6a55223ec4d1450ceff313859f39dc77361
Lucas Theisen <lucastheisen@pastdev.com>
2018-11-11 16:50:01 -0500

    modified ci to use test proj for tests

--- ci.sh
+++ ci.sh
@@ -9,8 +9,8 @@ DIR=$(dirname "$(readlink -f "$0")")
 "$DIR/build.sh"
 
 PROJECTS=" \
-  Pastdev.Dotnet.Xunit.Math  
-  Pastdev.Dotnet.Xunit.Command  
+  Pastdev.Dotnet.Xunit.Math.Test
+  Pastdev.Dotnet.Xunit.Math.IntegrationTest
   " \
   "$DIR/test.sh"
 
