f154e83a50c0b155da91933a7c50756b52d209c4
Lucas Theisen <lucastheisen@pastdev.com>
2018-11-11 16:28:24 -0500

    updated test.sh, added release.sh

--- release.sh
+++ release.sh
@@ -1,3 +1,64 @@
 #!/bin/bash
 
+set -e
 
+DIR=$(dirname "$(readlink -f "$0")")
+ARTIFACTS_DIR="${TEST_RESULTS_DIR:-$DIR/build}"
+REMOTE_REPO_URL="${REMOTE_REPO_URL:-$(git remote get-url origin)}"
+NUGET_API_KEY="${NUGET_API_KEY:+--api-key $NUGET_API_KEY}"
+NUGET_SOURCE="${NUGET_SOURCE:+--source $NUGET_SOURCE}"
+
+if dotnet tool list --tool-path . | grep nbgv > /dev/null 2>&1; then
+  echo "Updating nbgv"
+  dotnet tool update --tool-path . nbgv
+else
+  echo "Installing nbgv"
+  dotnet tool install --tool-path . nbgv
+fi
+
+NUGET_VERSION=$(./nbgv get-version | grep "NuGet" | sed 's/NuGet.*:[[:space:]]*//')
+echo "Version: [$NUGET_VERSION]"
+GIT_TAG="v$NUGET_VERSION"
+echo "Git tag: [$GIT_TAG]"
+
+if git remote get-url __CI__; then
+  git remote set-url __CI__ "$REMOTE_REPO_URL"
+else
+  git remote add __CI__ "$REMOTE_REPO_URL"
+fi
+
+git tag -am "Release $GIT_TAG" "$GIT_TAG"
+git push __CI__ "$GIT_TAG"
+
+dotnet pack \
+  --no-restore \
+  --no-build \
+  --include-symbols \
+  --include-source \
+  --output $ARTIFACTS_DIR
+
+FAILED=0
+for NUPKG in $ARTIFACTS_DIR/*.nupkg; do
+  echo "Pushing $NUPKG"
+
+  if [[ "$NUPKG" =~ symbols.nupkg$ ]]; then
+    echo "Skipping symbols package [$NUPKG]"
+    continue
+  fi
+
+  set +e
+  dotnet nuget push $NUGET_API_KEY $NUGET_SOURCE $NUPKG
+  EXIT_CODE=$?
+  set -e
+
+  if [ $EXIT_CODE != 0 ]; then
+    FAILED=$EXIT_CODE
+  fi
+done
+
+if [ 0 != $FAILED ]; then
+  echo "Some push failed: $FAILED"
+  exit $FAILED
+fi
+
+echo "Done!"
--- test.sh
+++ test.sh
@@ -5,12 +5,32 @@ set -e
 DIR=$(dirname "$(readlink -f "$0")")
 TEST_ROOT_DIR="${TEST_ROOT_DIR:-test}"
 TEST_RESULTS_DIR="${TEST_RESULTS_DIR:-$DIR/build/}"
-PROJECT=${PROJECT:-Pastdev.Dotnet.Xunit.Math.Test}
-PROJECT_RESULTS_FILE="$PROJECT.trx"
+PROJECTS=${PROJECTS:-Pastdev.Dotnet.Xunit.Math.Test}
 
-rm -f "$TEST_RESULTS_DIR/$PROJECT_RESULTS_FILE"
+FAILED=0
+for PROJECT in $PROJECTS; do
+  PROJECT_RESULTS_FILE="$PROJECT.trx"
 
-dotnet test \
+  rm -f "$TEST_RESULTS_DIR/$PROJECT_RESULTS_FILE"
+
+  set +e
+  dotnet test \
+    --no-restore \
+    --no-build \
     --logger "trx;LogFileName=$PROJECT_RESULTS_FILE" \
     --results-directory "$TEST_RESULTS_DIR" \
     "$TEST_ROOT_DIR/$PROJECT/$PROJECT.csproj"
+  EXIT_CODE=$?
+  set -e
+
+  if [ $EXIT_CODE != 0 ]; then
+    FAILED=$EXIT_CODE
+  fi
+done
+
+if [ 0 != $FAILED ]; then
+  echo "Some tests failed: $FAILED"
+  exit $FAILED
+fi
+
+echo "Done!"
